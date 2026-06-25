51a03cd5dcb2fe00fb350e9cd8297a8e32ca3dcc
Pori-chan <s.porichan@gmail.com>
2026-06-25 18:59:51 +0900

    価格（模擬）のリアルタイム変動

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { Header } from "./components/Header";
 import { MarketTable } from "./components/MarketTable";
 import { StatsPanel } from "./components/StatsPanel";
-import { useState } from "react";
+import { useEffect,useState } from "react";
 import { mockMarketData } from "./data/mockMarketData";
 
 function App() {
   const [marketData, setMarketData] = useState(mockMarketData);
+  useEffect(()=>{
+    const timer = setInterval(()=>{
+      setMarketData((currentData)=>{
+
+        const updatedData = currentData.map((item)=>({
+          ...item,
+          price:Number((item.price+(Math.random()-0.5)*2).toFixed(2)),
+        }));
+
+        return updatedData;
+      });
+    },1000);
+
+    return ()=> clearInterval(timer);
+  },[]);
 
   return (
     <div>
